500800e5971e554f37c128879cef838ccd40a687
mauricio ledesma <101837354+mau500882@users.noreply.github.com>
2024-10-21 20:06:14 -0300

    Create entrenadores.cs

---
+++ entrenadores.cs
@@ -0,0 +1,22 @@
+/*
+ * Creado por SharpDevelop.
+ * Usuario: mauri
+ * Fecha: 21/10/2024
+ * Hora: 19:28
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace proyecto_final
+{
+	/// <summary>
+	/// Description of entrenadores.
+	/// </summary>
+	public class entrenadores
+	{
+		public entrenadores()
+		{
+		}
+	}
+}
