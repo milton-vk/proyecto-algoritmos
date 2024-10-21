3517860a65bf78002ba29ff890843cec48bf60bf
mauricio ledesma <101837354+mau500882@users.noreply.github.com>
2024-10-21 20:04:46 -0300

    Create club.cs

---
+++ club.cs
@@ -0,0 +1,22 @@
+
+ * Creado por SharpDevelop.
+ * Usuario: mauri
+ * Fecha: 21/10/2024
+ * Hora: 19:22
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace proyecto_final
+{
+	/// <summary>
+	/// Description of club.
+	/// </summary>
+	public class club
+	{
+		public club()
+		{
+		}
+	}
+}
