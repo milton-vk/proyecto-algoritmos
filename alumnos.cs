4e5f4a622f71dd093a3e0e84a2367ca198aa0ef6
mauricio ledesma <101837354+mau500882@users.noreply.github.com>
2024-10-21 20:05:32 -0300

    Create alumnos.cs

---
+++ alumnos.cs
@@ -0,0 +1,31 @@
+/*
+ * Creado por SharpDevelop.
+ * Usuario: mauri
+ * Fecha: 21/10/2024
+ * Hora: 19:27
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace proyecto_final
+{
+	/// <summary>
+	/// Description of alumnos.
+	/// </summary>
+	public class alumnos
+	{
+		public alumnos()
+		{
+			private string nombre;
+			private int edad;
+			private string categoria;
+			private string dni;
+			private string deporte;
+			private string ultimo_mes_pago;
+			private bool socio;
+			
+		}
+		public void inscripcion
+	}
+}
